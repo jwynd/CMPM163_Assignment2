bd77113d24837c527a7f9c12b5c21fbd36563570
jwynd <18224844+jwynd@users.noreply.github.com>
2019-05-12 16:51:48 -0700

    Commit after second git reset to try and fix issues

---
+++ Assignment2/Assets/Scripts/CreatePerlinNoise.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePerlinNoise : MonoBehaviour
+{
+    [SerializeField] private int pixWidth;
+    [SerializeField] private int pixHeight;
+    private float xOrg;
+    private float yOrg;
+    private float scale;
+    private Texture2D noiseTex;
+    private Color[] pix;
+    private Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        print(mat);
+        xOrg = Random.Range(0.0f, 100.0f);
+        yOrg = Random.Range(0.0f, 100.0f);
+        scale = Random.Range(3.0f, 8.0f);
+        noiseTex = new Texture2D(pixWidth, pixHeight);
+        pix = new Color[noiseTex.width * noiseTex.height];
+        CalcNoise();
+        mat.SetTexture("_HeightMapTex", noiseTex);
+    }
+    
+
+    private void CalcNoise()
+    {
+        // For each pixel in the texture...
+        float y = 0.0F;
+
+        while (y < noiseTex.height)
+        {
+            float x = 0.0F;
+            while (x < noiseTex.width)
+            {
+                float xCoord = xOrg + x / noiseTex.width * scale;
+                float yCoord = yOrg + y / noiseTex.height * scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
+                x++;
+            }
+            y++;
+        }
+
+        // Copy the pixel data to the texture and load it into the GPU.
+        noiseTex.SetPixels(pix);
+        noiseTex.Apply();
+    }
+
+    /*void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture){
+        mat.SetTexture("_HeightMapTex", noiseTex);
+        Graphics.Blit(sourceTexture, destTexture, mat);
+    }*/
+}
--- Assignment2/Assets/Scripts/Rotate.cs
+++ Assignment2/Assets/Scripts/Rotate.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
     [SerializeField][Range(0.0f, 100.0f)] private float speed;
+    private RenderEffectBloom reb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        reb = GameObject.Find("Main Camera").GetComponent<RenderEffectBloom>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.A))
         transform.Rotate(0, Time.deltaTime*speed, 0);
+        else if(Input.GetKey(KeyCode.D))
+        transform.Rotate(0, -Time.deltaTime*speed, 0);
+        if(Input.GetKey(KeyCode.W))
+        transform.Rotate(Time.deltaTime*speed, 0, 0);
+        else if(Input.GetKey(KeyCode.S))
+        transform.Rotate(-Time.deltaTime*speed, 0, 0);
+
+        if(Input.GetKey(KeyCode.LeftArrow) && reb.BloomFactor > 0.1f) reb.BloomFactor -= Time.deltaTime*10;
+        else if(Input.GetKey(KeyCode.RightArrow) && reb.BloomFactor < 100.0f) reb.BloomFactor += Time.deltaTime*10;
     }
 }
