9c66a5e9d54e2ff4a5b1d0643db3f0bae5834933
jwynd <18224844+jwynd@users.noreply.github.com>
2019-05-12 17:00:30 -0700

    Scene Control

---
+++ Assignment2/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    private string[] scenes = {"Part1","Part2"};
+    private string thisScene;
+    private int otherScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisScene = SceneManager.GetActiveScene().name;
+        otherScene = thisScene == "Part1"?1:0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene(scenes[otherScene], LoadSceneMode.Single);
+        }
+    }
+}
